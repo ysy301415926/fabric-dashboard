6261c758a25e8d9e7791ce1223ba7edc046cb30b
zhanglei <zhanglei>
2018-05-18 19:20:17 +0800

    Transaction id

--- src/test/java/com/jd/fabric/manager/ChaincodeManagerTest.java
+++ src/test/java/com/jd/fabric/manager/ChaincodeManagerTest.java
@@ -3,6 +3,7 @@ package com.jd.fabric.manager;
 import com.jd.fabric.dashboard.manager.ChaincodeManager;
 import org.hyperledger.fabric.sdk.BlockInfo;
 import org.hyperledger.fabric.sdk.BlockchainInfo;
+import org.hyperledger.fabric.sdk.TransactionInfo;
 import org.junit.Test;
 
 import java.util.Map;
@@ -42,9 +43,14 @@ public class ChaincodeManagerTest {
         try{
             FabricManagerTest fabricManagerTest = new FabricManagerTest();
             ChaincodeManager chaincodeManager = fabricManagerTest.getChaincodeManager();
-            BlockInfo blockInfo = chaincodeManager.queryBlockByNumber(16L);
+            BlockInfo blockInfo = chaincodeManager.queryBlockByNumber(17L);
             System.out.println(blockInfo.getBlockNumber());
-            //System.out.println(blockInfo);
+            System.out.println(blockInfo.getBlock());
+            System.out.println(new String(blockInfo.getDataHash(),"utf-8"));
+            System.out.println("-----------------------------------------");
+            System.out.println(new String(blockInfo.getBlock().getData().getData(0).toByteArray(),"utf-8"));
+
+            //2129d1123d27852607d52c6ef44a0088dd9da1b1d6f204fab837ca12fa065a85
 
         }catch(Exception e){
             e.printStackTrace();
@@ -60,6 +66,21 @@ public class ChaincodeManagerTest {
             ChaincodeManager chaincodeManager = fabricManagerTest.getChaincodeManager();
             BlockchainInfo blockchainInfo = chaincodeManager.queryBlockchainInfo();
             System.out.println(blockchainInfo.getBlockchainInfo());
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    @Test
+    public  void queryTransactionByID(){
+        try{
+            FabricManagerTest fabricManagerTest = new FabricManagerTest();
+            ChaincodeManager chaincodeManager = fabricManagerTest.getChaincodeManager();
+            TransactionInfo transactionInfo = chaincodeManager.queryTransactionByID("2129d1123d27852607d52c6ef44a0088dd9da1b1d6f204fab837ca12fa065a85");
+            System.out.println(new String(transactionInfo.getEnvelope().toByteArray(),"utf-8"));
 
         }catch(Exception e){
             e.printStackTrace();
